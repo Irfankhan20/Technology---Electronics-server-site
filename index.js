9dbc4600a5f237bcdc261a9df2d6ec908dceb529
Irfankhan20 <sipathan96@gmail.com>
2023-10-21 16:23:10 +0600

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,12 +9,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// assignment10
-// TXGSSAaNAR1ZZFO7
 
 //-----------------------------------------------------------------------------
-
-const uri = "mongodb+srv://assignment10:TXGSSAaNAR1ZZFO7@project1.4j1y0pd.mongodb.net/?retryWrites=true&w=majority";
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@project1.4j1y0pd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,6 +28,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // collection 
+    const brandCollection = client.db('brandDB').collection('brand');
+
+    // post 
+    app.post('/brand',async(req,res)=>{
+        const newBrand = req.body;
+        const result = await brandCollection.insertOne(newBrand);
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
