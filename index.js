bff27e9374922d6a2058d68753554a80e4ef608f
Irfankhan20 <sipathan96@gmail.com>
2023-10-21 20:32:51 +0600

    delete operation done

--- index.js
+++ index.js
@@ -46,6 +46,15 @@ async function run() {
         res.send(result);
     })
 
+    //delete
+    app.delete('brand/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await brandCollection.deleteOne(query);
+        res.send(result);
+
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
